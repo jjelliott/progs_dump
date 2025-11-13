3561e50a8030d4b8d1416867fdce3ad71b2aa844
iLike80sRock <jesseelliott@outlook.com>
2025-11-13 16:13:38 -0500

    Hopefully fix routing

--- docsrc/src/routes/manual/ManualNav.svelte
+++ docsrc/src/routes/manual/ManualNav.svelte
@@ -4,13 +4,14 @@
     import MonstersNav from "./monsters/nav.svelte";
 
     let {singlePage = false} = $props();
+		console.log(`manualnav singlePage: ${singlePage}`);
 </script>
 
 <nav>
     <ul style="list-style: none">
         <ManualNavLink page="/manual" tag="#installation" tagOnly={singlePage}>Installation</ManualNavLink>
         <ManualNavLink page="/manual" tag="#dev-notes" tagOnly={singlePage} indent={1}>Development Notes</ManualNavLink>
-        <SpawnflagNav singlePage/>
-        <MonstersNav singlePage/>
+        <SpawnflagNav singlePage={singlePage}/>
+        <MonstersNav singlePage={singlePage}/>
     </ul>
 </nav>
--- docsrc/src/routes/manual/spawnflags/nav.svelte
+++ docsrc/src/routes/manual/spawnflags/nav.svelte
@@ -1,7 +1,9 @@
 <script>
 import ManualNavLink from "../ManualNavLink.svelte";
- let {singlePage} = $props();
+ let {singlePage = false} = $props();
+
+console.log(`spawnflagnav singlePage: ${singlePage}`);
 </script>
 <ManualNavLink page="/manual/spawnflags" tag="#spawnflags" tagOnly={singlePage}>Spawnflags</ManualNavLink>
-        <ManualNavLink page="/manual/spawnflags" tag="#spawnflags-trigger-spawn" tagOnly={singlePage} indent={1}>Trigger Spawned Monsters</ManualNavLink>
-        <ManualNavLink page="/manual/spawnflags" tag="#spawnflags-appearance-flags" tagOnly={singlePage} indent={1}>Appearance Flags</ManualNavLink>
+<ManualNavLink page="/manual/spawnflags" tag="#spawnflags-trigger-spawn" tagOnly={singlePage} indent={1}>Trigger Spawned Monsters</ManualNavLink>
+<ManualNavLink page="/manual/spawnflags" tag="#spawnflags-appearance-flags" tagOnly={singlePage} indent={1}>Appearance Flags</ManualNavLink>
